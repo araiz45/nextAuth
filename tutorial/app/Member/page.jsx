91107535de7584be4dfb4f427078e408f54048e1
araiz45 <araizzahid45@gmail.com>
2024-01-29 00:44:36 +0500

    intail commit

--- tutorial/app/Member/page.jsx
+++ tutorial/app/Member/page.jsx
@@ -1,7 +1,17 @@
-export default function Member() {
+import { getServerSession } from "next-auth";
+import { options } from "../api/auth/[...nextauth]/options";
+import { redirect } from "next/navigation";
+
+export default async function Member() {
+  const session = await getServerSession(options);
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/Member");
+  }
   return (
     <div>
       <h1>Member Server Session</h1>
+      <p>{session?.user?.email}</p>
+      <p>{session?.user?.role}</p>
     </div>
   );
 }
